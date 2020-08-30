cc4588889099c3b774d4b1c03e2661f8f7b6fd46
Bugra Aydingoz <bugraaydingoz@gmail.com>
2020-08-30 19:08:11 +0200

    Revert dynamic favicon

--- hooks/useTheme.js
+++ hooks/useTheme.js
@@ -1,14 +1,5 @@
 import { useState } from 'react'
 
-function changeFavicon(theme) {
-  const link =
-    document.querySelector("link[rel*='icon']") ||
-    document.createElement('link')
-  link.type = 'image/x-icon'
-  link.href = `/favicon-${theme}.ico`
-  document.getElementsByTagName('head')[0].appendChild(link)
-}
-
 // TODO: Set theme based on media query
 export function useTheme() {
   const [theme, setTheme] = useState('light')
@@ -17,11 +8,9 @@ export function useTheme() {
     if (theme === 'light') {
       setTheme('dark')
       document.documentElement.setAttribute('data-theme', 'dark')
-      changeFavicon('dark')
     } else {
       setTheme('light')
       document.documentElement.setAttribute('data-theme', 'light')
-      changeFavicon('light')
     }
   }
 
